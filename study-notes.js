e5cf31ce55993f954141e8fb3eb478dc24987c62
Jean Carlo Sinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2022-07-24 00:00:21 -0500

    Update study-notes.js

--- study-notes.js
+++ study-notes.js
@@ -159,3 +159,25 @@ if (bmi >= 25) {
 }
 
 bmiCalculator(63,1.65);
+
+// Here are a serie of popular Coding Challenges.
+// #1 FizzBuzz
+var output = [];
+var count = 1;
+function fizzBuzz() {
+
+if (count % 3 === 0 && count % 5 ===0) {
+    output.push("Fizz");
+} 
+else if(count % 3 ===0) {
+    output.push("Buzz");
+}   
+else if(count % 5 ===0) {
+    output.push("Buzz");
+}   
+else {
+    output.push(count);
+}
+count++; 
+    console.log(output);
+}
