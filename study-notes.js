956f2bc7e68ce7e5c7da7c46679719a0bc4c7bfd
Jean Carlo Sinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2022-07-24 01:51:36 -0500

    Update study-notes.js

--- study-notes.js
+++ study-notes.js
@@ -164,22 +164,26 @@ bmiCalculator(63,1.65);
 // #1 FizzBuzz
 var output = [];
 var count = 1;
+
 function fizzBuzz() {
+    
+    while(count <= 100) {
 
 if (count % 3 === 0 && count % 5 ===0) {
     output.push("Fizz");
-} 
-else if(count % 3 ===0) {
+        }
+    else if(count % 3 ===0) {
     output.push("Buzz");
-}   
-else if(count % 5 ===0) {
+    } else if(count % 5 ===0) {
     output.push("Buzz");
-}   
-else {
+    }
+    else {
     output.push(count);
-}
-count++; 
-    console.log(output);
+    }    
+    count++;
+   
+    }
+    console.log(output); 
 }
 
 // #2 Random Selection to see who will pay the Lunch
